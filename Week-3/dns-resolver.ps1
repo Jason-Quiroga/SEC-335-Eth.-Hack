5eef8352216397ab908aa606f4283467f884f92e
Jason Quiroga <jason.quiroga@mymail.champlain.edu>
2022-02-08 10:09:26 -0500

    Completed Assignment 3.1 for PowerShell DNS Resolver

---
+++ Week-3/dns-resolver.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: DNS Resolver using Powershell
+# Created by Jason Quiroga
+
+param($network, $server)
+
+for (($i = 1); $i -le 254; $i++)
+{
+    $ipaddress = "$network.$i"
+
+    $resolved = Resolve-DnsName -DnsOnly $ipaddress -Server $server -ErrorAction Ignore | Select-Object -ExpandProperty NameHost -first 1
+
+    if ($resolved -ne $null)
+    {
+        write-host $ipaddress $resolved
+    }
+}
